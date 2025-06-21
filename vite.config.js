78af28008572c8f22aab5d800eb9fa82a1b1596d
viclok <110517011+viclok@users.noreply.github.com>
2025-06-22 00:20:15 +1000

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        form: resolve(__dirname, 'form.html'),
+      }
+    }
+  }
+});
