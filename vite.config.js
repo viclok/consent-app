0d6b678e39271daeb0cfb0f8e2a2ad7b940a0a41
viclok <110517011+viclok@users.noreply.github.com>
2025-06-23 23:13:25 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         form: resolve(__dirname, 'form.html'),
+        clinic: resolve(__dirname, 'clinic.html'),
+        patient: resolve(__dirname, 'patient.html'),
       }
     }
   }
